c66e114d528751323448bd57356145a93ca854d1
Mohit singh <78977366+singhmohit14072002@users.noreply.github.com>
2023-12-20 21:18:37 +0530

    added the day4 learning

---
+++ Day4/calculator.py
@@ -0,0 +1,23 @@
+num1 = 10
+num2 = 20
+
+def addition():
+    add = num1 + num2
+    print(add)
+
+def subtraction():
+    s = num1 - num2
+    print(s)
+
+def multiplication():
+    mul = num1 * num2
+    print(mul)
+
+def division():
+    dev = num1 / num2
+    print(dev)
+
+
+addition(); subtraction(); multiplication(); division();
+
+# In this vedio implementation of function wwe have done and try to understand  wwhy function are important
