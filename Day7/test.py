4ecbd9f65c1780dbe407d5d807446c9758a04b5a
Mohit singh <78977366+singhmohit14072002@users.noreply.github.com>
2023-12-23 03:07:57 +0530

    added the test file

---
+++ Day7/test.py
@@ -0,0 +1,10 @@
+user_input = input("Give the Server Name: ")
+
+if user_input == "t2.micro":
+    print("Ok we will create instaces for you!")
+
+elif  user_input == "t3.micro":
+    print("Ok we will create instaces for you but it charge more money!")
+
+else:
+    print("Server are costly and took more money")
