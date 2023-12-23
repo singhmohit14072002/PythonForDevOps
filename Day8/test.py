da2874af87035bfdaf2be2a28594d6e51df8a11d
Mohit singh <78977366+singhmohit14072002@users.noreply.github.com>
2023-12-23 18:40:57 +0530

    added the day8 files

---
+++ Day8/test.py
@@ -0,0 +1,11 @@
+studensts_names = ["heyy", "howdy"] 
+studensts_names.append("whatsaapproved")
+print(studensts_names)    
+
+# for tuple we use () instead of []
+
+name_list = ["Alice", "Bob", "Charlie"]
+removed_name = name_list.pop(1)  # Removes the element at index 1 ("Bob")
+print("Removed:", removed_name)
+print("Updated name list:", name_list)
+
